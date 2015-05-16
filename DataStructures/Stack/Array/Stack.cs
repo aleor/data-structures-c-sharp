1cfd8a085e6cf465d2813533a9601bf2ef4523c8
arvalea <arvalea@gmail.com>
2015-05-16 18:23:09 +0300

    added stack on array

--- DataStructures/Stack/Array/Stack.cs
+++ DataStructures/Stack/Array/Stack.cs
@@ -6,7 +6,76 @@ using System.Threading.Tasks;
 
 namespace DataStructures.Stack.Array
 {
-    class Stack
+    public class Stack<T> : IEnumerable<T>
     {
+        /// <summary>
+        /// Create and array which size will be changed automatically
+        /// </summary>
+        private T[] _items = new T[0];
+
+        /// <summary>
+        /// Here we will keep current number of elements in the array
+        /// </summary>
+        private int _size;
+
+
+        public void Push (T item)
+        {
+            if (_items.Length == _size)
+            {
+                int newSize = _size == 0 ? 4 : _size*2;
+                
+                var newArray = new T[newSize];
+                _items.CopyTo(newArray, 0);
+                _items = newArray;
+            }
+
+            _items[_size] = item;
+            _size++;
+        }
+
+        public T Pop()
+        {
+            if (_items.Length == 0)
+            {
+                throw new InvalidOperationException("Stack is empty");
+            }
+
+            _size--;
+
+            return _items[_size];
+        }
+
+        public T Peek()
+        {
+            if (_items.Length == 0)
+            {
+                throw new InvalidOperationException("Stack is empty");
+            }
+
+            return _items[_size--];
+        }
+
+
+        public void Clear()
+        {
+            _size = 0;
+        }
+
+        public int Count { get { return _items.Length; } }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = _size - 1; i >= 0; i--)
+            {
+                yield return _items[i];
+            }
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
     }
+
 }
